9d0f93c0162ef91ab449c502a0fd74afb64119be
gesmirshyti <gersi.shyti01@gmail.com>
2023-08-26 12:49:17 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-// import {useState} from "react";  
-import PropItUp from "./components/PropItUp";
+import UserForm from "./components/UserForm";
 function App() {
-  // const [age,setAge] = useState();
   return (
     <>
-
-    <PropItUp firstName={"Gesmir"} lastName={"Shyti"} initialAge={21} hairColor={"Black"} />
+    <UserForm   />
     </>
   );
 }
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from  'react';
+    
+const UserForm = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+    const [confirmPassword, setConfirmPassword] = useState(""); 
+    
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+        
+        // shorthand ES6 syntax for building an object - see notes above
+        const newUser = { firstName,lastName, email, password ,confirmPassword};
+        console.log("Welcome", newUser);
+    	setFirstName("");
+        setLastName("");
+    	setEmail("");
+    	setPassword("");
+        setConfirmPassword("");
+    };
+    
+    return(
+        <form onSubmit={ createUser }>
+            <div>
+                <label>FirstName: </label> 
+                <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>LastName: </label> 
+                <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" value={password} onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="text" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+            <div>
+            <p>First Name : {firstName}</p>
+            <p>Last Name : {lastName}</p>
+            <p>Email : {email}</p>
+            <p>password : {password}</p>
+            <p>Confirm Password : {confirmPassword}</p>
+            </div>
+        </form>
+    );
+};
+    
+export default UserForm;
