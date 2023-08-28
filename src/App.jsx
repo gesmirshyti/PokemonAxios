a7fdc62d9e7281ba2008f096a1c4ac4765b6522d
gesmirshyti <gersi.shyti01@gmail.com>
2023-08-28 23:16:10 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
-import React,{useState} from "react";
-import Tabs from "./components/TabComponent";
-import { Provider } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
-const engine = new Styletron();
+import React, { useState } from "react";
+import './App.css'
+
 function App() {
-  const tabsData = [
-    { label: 'Tab 1', content: 'Content for Tab 1', onClick: () => console.log('Tab 1 clicked') },
-    { label: 'Tab 2', content: 'Content for Tab 2', onClick: () => console.log('Tab 2 clicked') },
-    { label: 'Tab 3', content: 'Content for Tab 3' },
-  ];
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+      completed: false,
+    };
+    setList([...list, newTodo]);
+    setInput(""); // Clear input box
+  };
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
+  const handleToggleComplete = (id) => {
+    const updatedList = list.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    setList(updatedList);
+  };
 
   return (
     <div>
-      <h1>Tab Component Example</h1>
-      <Tabs items={tabsData} />
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id} className={todo.completed ? "completed" : ""}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(todo.id)}
+            />
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
-
   );
 }
 
