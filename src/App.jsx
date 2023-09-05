ad793cb11aadacb6445098dd786c5e6e1ea03374
gesmirshyti <gersi.shyti01@gmail.com>
2023-09-05 12:19:23 +0200

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css'
+// import PokemonApi from "./components/PokemonAPI";
 
 function App() {
-    function tossCoin() {
-        // Simulate a coin toss by generating random "heads" or "tails"
-        return Math.random() > 0.5 ? "heads" : "tails";
-    }
-    
-    function fiveHeadsAsync() {
-        return new Promise((resolve, reject) => {
-            let headsCount = 0;
-            let attempts = 0;
-    
-            function flipCoin() {
-                attempts++;
-                let result = tossCoin();
-                console.log(`${result} was flipped`);
-                if (result === "heads") {
-                    headsCount++;
-                } else {
-                    headsCount = 0;
-                }
-    
-                if (headsCount === 5) {
-                    resolve(`It took ${attempts} tries to flip five "heads"`);
-                } else if (attempts > 100) {
-                    reject("Coin was flipped more than 100 times without getting five heads.");
-                } else {
-                    flipCoin();
-                }
-            }
-    
-            flipCoin();
-        });
-    }
-    
-    fiveHeadsAsync()
-        .then(result => {
-            console.log(result);
-            console.log("This is run after the fiveHeadsAsync function completes");
-        })
-        .catch(error => {
-            console.error("An error occurred:", error);
-        });
-    
-    
-    return (
-      <div className="App"></div>
-      );
     }
     
     export default App;
